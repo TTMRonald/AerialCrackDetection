d0548d018132bf21f04dbf349241a91aa4aa13e7
TTMRonald <wb1581875472@163.com>
2017-07-27 02:55:44 +0800

    Update VGG_16.py

--- networks/VGG_16.py
+++ networks/VGG_16.py
@@ -59,7 +59,6 @@ def nn_base(input_tensor=None, trainable=False):
 def rpn(base_layers,num_anchors):
 
     x = Convolution2D(512, (3, 3), padding='same', activation='relu', kernel_initializer='normal', name='rpn_conv1')(base_layers)
-
     x_class = Convolution2D(num_anchors, (1, 1), activation='sigmoid', kernel_initializer='uniform', name='rpn_out_class')(x)
     x_regr = Convolution2D(num_anchors * 4, (1, 1), activation='linear', kernel_initializer='zero', name='rpn_out_regress')(x)
 

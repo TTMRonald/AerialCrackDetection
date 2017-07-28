8d75452c757ee5adbd560c975542d9a178718bbf
TTMRonald <wb1581875472@163.com>
2017-07-28 12:54:50 +0800

    Update VGG_16.py

--- networks/VGG_16.py
+++ networks/VGG_16.py
@@ -65,6 +65,7 @@ def rpn(base_layers,num_anchors):
 
 def classifier(base_layers, input_rois, batch_size, nb_classes = 3, trainable=False):
 
+    
     # compile times tend to be very high, so we use smaller ROI pooling regions to workaround
     if K.backend() == 'tensorflow':
         pooling_regions = 14
